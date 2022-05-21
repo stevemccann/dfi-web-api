739023c212f400956010abed3bf1f0b5a119850a
Steve McCann <steve.mccann@blendedperspectives.com>
2022-05-21 13:19:08 +0700

    update script

--- init-defi.sh
+++ init-defi.sh
@@ -2,11 +2,13 @@
 
 # This script downloads a snapshot of defichain to speed up the time it takes to sync a new node. It also downloads the CLI for use in the container.
 
-# Get latest snapshot from https://defi-snapshots-europe.s3.eu-central-1.amazonaws.com/index-1.8.0.txt where 1.8.0 is the main CLI version
+# Get latest snapshot from https://defi-snapshots.s3.ap-southeast-1.amazonaws.com/index.txt
 # Can also check https://github.com/DeFiCh/ain/releases
-SNAPSHOT='snapshot-mainnet-1481625.zip'
+SNAPSHOT='snapshot-mainnet-1894199.zip'
 DEFI_DIRECTORY='data/dfi-node/defi'
-CLI_VERSION='2.3.1'
+CLI_VERSION='2.7.1'
+SNAPSHOT_BASE_URL='https://defi-snapshots.s3.ap-southeast-1.amazonaws.com'
+#SNAPSHOT_BASE_URL='https://defi-snapshots-europe.s3.eu-central-1.amazonaws.com'
 
 CLI_FILE=defichain-${CLI_VERSION}-x86_64-pc-linux-gnu.tar.gz
 
@@ -21,18 +23,13 @@ fi
 mkdir -p data/dfi-node
 
 echo "Downloading snapshot"
-wget https://defi-snapshots-europe.s3.eu-central-1.amazonaws.com/${SNAPSHOT}
-
+wget ${SNAPSHOT_BASE_URL}/${SNAPSHOT}
 if [ -f "$SNAPSHOT" ]; then
     echo "Unzipping $SNAPSHOT"
     unzip ${SNAPSHOT} -d ${DEFI_DIRECTORY}
 fi
 
 echo "Downloading defi cli"
-SNAPSHOT_BASE_URL='https://defi-snapshots.s3.ap-southeast-1.amazonaws.com'
-#wget https://defi-snapshots-europe.s3.eu-central-1.amazonaws.com/${SNAPSHOT}
-wget ${SNAPSHOT_BASE_URL}/${SNAPSHOT}
-
 if [ -f "$CLI_FILE" ]; then
     echo "Untarring $CLI_FILE"
     # folder in tar is defichain-<VERSION> i.e. defichain-1.8.2
